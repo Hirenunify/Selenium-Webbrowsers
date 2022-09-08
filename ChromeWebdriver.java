73db813272e73d08131a23e3bb183d97661e3fa9
Hirenunify <109555324+Hirenunify@users.noreply.github.com>
2022-09-08 01:53:22 +0100

    Add files via upload

---
+++ ChromeWebdriver.java
@@ -0,0 +1,16 @@
+package SeleniumSession;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ChromeWebdriver {
+    public static void main(String[] args) {
+
+        System.setProperty ("webdriver.chrome.driver","C:\\Software\\Driver\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+
+
+            }
+}
