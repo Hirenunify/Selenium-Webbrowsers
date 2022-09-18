5dc4b6645d74325c04a584016863ab56693cac07
Hirenunify <109555324+Hirenunify@users.noreply.github.com>
2022-09-18 19:35:02 +0100

    Add files via upload

---
+++ Login.java
@@ -0,0 +1,21 @@
+package SeleniumSession;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Login {
+
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://demo.nopcommerce.com/");
+        driver.findElement(By.className("ico-login")).click();
+        driver.findElement(By.id("Email")).sendKeys("hirenbhatt2511@gmail.com");
+        driver.findElement(By.id("Password")).sendKeys("123456");
+        driver.findElement(By.id("RememberMe")).click();
+        driver.findElement(By.xpath("/html/body/div[6]/div[3]/div/div/div/div[2]/div[1]/div[2]/form/div[3]/button")).click();
+    }
+}
